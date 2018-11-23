05374a7e1426503bb94e279deeb1bd016622a428
Damien Delay <Damien.Delay@xlntelecom.co.uk>
2018-11-23 14:24:15 +0000

    no message

--- remove-bloat.sh
+++ remove-bloat.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+# remove applications
+sudo apt-get --yes remove --purge minecraft-pi 
+sudo apt-get --yes remove --purge scratch
+sudo apt-get --yes remove --purge wolfram-engine
+sudo apt-get --yes remove --purge debian-reference-*
+sudo apt-get --yes remove --purge epiphany-browser*
+sudo apt-get --yes remove --purge sonic-pi 
+sudo apt-get --yes remove --purge supercollider*
+sudo apt-get --yes remove --purge greenfoot
+sudo apt-get --yes remove --purge bluej
+sudo apt-get --yes remove --purge sense-hat
+sudo apt-get --yes remove --purge sense-emu-tools
+sudo apt-get --yes remove --purge scratch2
+sudo apt-get --yes remove --purge nodered
+sudo apt-get --yes remove --purge libreoffice*
+sudo apt-get --yes remove --purge claws-mail
+sudo apt-get --yes remove --purge python3-thonny
+sudo apt-get --yes remove --purge geany*
 
-# Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle)
-sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview -y
+#remove unused directory
+rm -r /home/pi/python_games/
 
 # Autoremove
 sudo apt-get autoremove -y
